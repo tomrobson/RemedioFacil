7b3d0685909202b05bc1306184fc681b3d071b83
Tom <tom.robson@hotmail.com>
2017-08-07 14:48:57 -0300

    add navegação entre as janelas

--- app/src/main/java/br/iesb/remediofacil/Cadastrar_Activity.java
+++ app/src/main/java/br/iesb/remediofacil/Cadastrar_Activity.java
@@ -1,7 +1,10 @@
 package br.iesb.remediofacil;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Cadastrar_Activity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class Cadastrar_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar);
+
+        Button b = (Button)  findViewById(R.id.buttonCadastrar);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Cadastrar_Activity.this, Login_Activity.class));
+            }
+        });
     }
 }
--- app/src/main/java/br/iesb/remediofacil/Login_Activity.java
+++ app/src/main/java/br/iesb/remediofacil/Login_Activity.java
@@ -1,7 +1,10 @@
 package br.iesb.remediofacil;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login_Activity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class Login_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button b = (Button)  findViewById(R.id.buttonLogin);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login_Activity.this, MainActivity.class));
+            }
+        });
+
+        Button br = (Button) findViewById(R.id.buttonRegistrar);
+        br.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login_Activity.this, Cadastrar_Activity.class));
+            }
+        });
     }
 }
