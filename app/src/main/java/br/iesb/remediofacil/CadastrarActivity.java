ba3bc4556793bb07d9ee86098d97a2c807e49b0c
Luan da Costa <luandacostadf@gmail.com>
2017-09-08 02:09:13 -0300

    Cadastro e Login com Email e Senha

--- app/src/main/java/br/iesb/remediofacil/CadastrarActivity.java
+++ app/src/main/java/br/iesb/remediofacil/CadastrarActivity.java
@@ -1,25 +1,67 @@
 package br.iesb.remediofacil;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CadastrarActivity extends AppCompatActivity {
+
+    private static final String TAG = "Teste:";
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private EditText email, password, name;
+    private Button buttonCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar);
 
-        Button b = (Button) findViewById(R.id.buttonCadastrar);
-        b.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+
+        email = (EditText) findViewById(R.id.cadastrar_email);
+        password = (EditText) findViewById(R.id.cadastrar_senha);
+        name = (EditText) findViewById(R.id.cadastrar_nome);
+        buttonCadastrar = (Button) findViewById(R.id.buttonCadastrar);
+
+        buttonCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(CadastrarActivity.this, LoginActivity.class));
-                finish();
+            public void onClick(View v) {
+                final ProgressDialog progressDialog = ProgressDialog.show(CadastrarActivity.this, "Aguarde", "Processando...", true);
+
+                mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+                                progressDialog.dismiss();
+
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(CadastrarActivity.this, "Cadastro efetuado com sucesso!", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(CadastrarActivity.this, LoginActivity.class));
+                                } else {
+                                    Log.e("Error : ", task.getException().toString());
+                                    Toast.makeText(CadastrarActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+                        });
             }
         });
+
     }
+
+
 }
--- app/src/main/java/br/iesb/remediofacil/LoginActivity.java
+++ app/src/main/java/br/iesb/remediofacil/LoginActivity.java
@@ -1,5 +1,6 @@
 package br.iesb.remediofacil;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -12,15 +13,20 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseNetworkException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "Teste LOGIN";
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private EditText login_email;
     private EditText login_senha;
+    private Button botaoLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,48 +35,38 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         mAuth = FirebaseAuth.getInstance();
 
-        Button b = (Button) findViewById(R.id.buttonLogin);
-        b.setOnClickListener(new View.OnClickListener() {
+        botaoLogin = (Button) findViewById(R.id.buttonLogin);
+        login_email = (EditText) findViewById(R.id.login_email);
+        login_senha = (EditText) findViewById(R.id.login_senha);
+
+
+        botaoLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+
+                final ProgressDialog progressDialog = ProgressDialog.show(LoginActivity.this, "Aguarde", "Processando...", true);
+
                 mAuth.signInWithEmailAndPassword(login_email.getText().toString(), login_senha.getText().toString())
-                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
-                                Log.d("01", "signInWithEmail:onComplete:" + task.isSuccessful());
+                                Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
 
-                                // If sign in fails, display a message to the user. If sign in succeeds
-                                // the auth state listener will be notified and logic to handle the
-                                // signed in user can be handled in the listener.
                                 if (!task.isSuccessful()) {
-                                    Log.w("01", "signInWithEmail:failed", task.getException());
-                                    Toast.makeText(LoginActivity.this, "Deu errado",
-                                            Toast.LENGTH_SHORT).show();
+                                    Log.w(TAG, "signInWithEmail:failed", task.getException());
+                                    Toast.makeText(LoginActivity.this, "Email ou senha icorretos!",Toast.LENGTH_SHORT).show();
                                 } else {
-                                    Toast.makeText(LoginActivity.this, "Deu certo",
-                                            Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(LoginActivity.this, "Logado com sucesso!",Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
                                 }
+
                             }
                         });
-
             }
         });
 
 
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
-    }
 
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
-        }
-    }
 }
