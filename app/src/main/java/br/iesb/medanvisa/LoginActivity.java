a4083b5f167d58d1fab7942b325fb248812ea678
Luan da Costa <luandacostadf@gmail.com>
2017-09-30 02:00:09 -0300

    Login com email e senha

--- app/src/main/java/br/iesb/medanvisa/LoginActivity.java
+++ app/src/main/java/br/iesb/medanvisa/LoginActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,34 +13,55 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button btnLogin;
     private EditText editEmail;
-    private EditText editassword;
+    private EditText editPassword;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         inicializaComponentes();
+        eventoClicks();
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth = Conexao.getFirebaseAuth();
     }
 
     private void eventoClicks() {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, SearchMedicineActivity.class));
+                String email = editEmail.getText().toString().trim();
+                String password = editPassword.getText().toString().trim();
+                login(email, password);
+            }
+        });
+    }
+
+    private void login(String email, String password) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    startActivity(new Intent(LoginActivity.this, SearchMedicineActivity.class));
+                } else {
+                    Toast.makeText(LoginActivity.this, "Email ou senha errados", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
 
     private void inicializaComponentes() {
         editEmail = (EditText) findViewById(R.id.login_email);
-        editassword = (EditText) findViewById(R.id.login_password);
+        editPassword = (EditText) findViewById(R.id.login_password);
         btnLogin = (Button) findViewById(R.id.login_btn_email_pass);
     }
 
