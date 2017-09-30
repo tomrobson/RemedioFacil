1e8a52279ad515211061daeddb7218a94626a162
Luan da Costa <luandacostadf@gmail.com>
2017-09-30 02:55:41 -0300

    Login com Google

--- app/src/main/java/br/iesb/medanvisa/LoginActivity.java
+++ app/src/main/java/br/iesb/medanvisa/LoginActivity.java
@@ -9,25 +9,49 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private Button btnLogin;
+    private Button btnLoginGoogle;
     private EditText editEmail;
     private EditText editPassword;
     private FirebaseAuth auth;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         inicializaComponentes();
+        conectaGoogleApi();
         eventoClicks();
     }
+
+    private void conectaGoogleApi() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+    }
+
 
     @Override
     protected void onStart() {
@@ -36,12 +60,54 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void eventoClicks() {
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = editEmail.getText().toString().trim();
                 String password = editPassword.getText().toString().trim();
                 login(email, password);
+            }
+        });
+
+        btnLoginGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginGoogle();
+            }
+        });
+
+
+    }
+
+    private void loginGoogle() {
+        Intent intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                GoogleSignInAccount signInAccount = result.getSignInAccount();
+                firebaseLoginGoogle(signInAccount);
+            }
+        }
+    }
+
+    private void firebaseLoginGoogle(GoogleSignInAccount signInAccount) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(signInAccount.getIdToken(), null);
+        auth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Intent intent = new Intent(LoginActivity.this, SearchMedicineActivity.class);
+                    startActivity(intent);
+                } else {
+                    alert("Falha na autenticação");
+                }
             }
         });
     }
@@ -63,6 +129,15 @@ public class LoginActivity extends AppCompatActivity {
         editEmail = (EditText) findViewById(R.id.login_email);
         editPassword = (EditText) findViewById(R.id.login_password);
         btnLogin = (Button) findViewById(R.id.login_btn_email_pass);
+        btnLoginGoogle = (Button) findViewById(R.id.login_btn_google);
     }
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        alert("Falha na conexão");
+    }
+
+    private void alert(String s) {
+        Toast.makeText(LoginActivity.this, s, Toast.LENGTH_LONG).show();
+    }
 }
--- app/src/main/java/br/iesb/medanvisa/SearchMedicineActivity.java
+++ app/src/main/java/br/iesb/medanvisa/SearchMedicineActivity.java
@@ -1,6 +1,7 @@
 package br.iesb.medanvisa;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -12,9 +13,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -22,14 +26,19 @@ public class SearchMedicineActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private TextView profileEmail;
+    private TextView profileName;
+    private ImageView profilePicture;
     private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
     private FirebaseUser user;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_medicine);
 
+        inicializaComponentes();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -50,7 +59,6 @@ public class SearchMedicineActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        inicializaComponentes();
 
 
     }
@@ -132,5 +140,7 @@ public class SearchMedicineActivity extends AppCompatActivity
 
     private void inicializaComponentes() {
         profileEmail = (TextView) findViewById(R.id.nav_header_search_medicine_profile_email);
+        profileName = (TextView) findViewById(R.id.nav_header_search_medicine_profile_name);
+        profilePicture = (ImageView) findViewById(R.id.nav_header_search_medicine_profile_picture);
     }
 }
