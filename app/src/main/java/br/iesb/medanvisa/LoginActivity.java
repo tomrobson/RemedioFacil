e8f4d89324c6cc571ed35b2b9e3c3b1017533504
Francimilton <francis.obsis@gmail.com>
2017-09-30 03:38:13 -0300

    Login com facebook

--- app/src/main/java/br/iesb/medanvisa/LoginActivity.java
+++ app/src/main/java/br/iesb/medanvisa/LoginActivity.java
@@ -4,11 +4,18 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.Call;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -19,25 +26,61 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthProvider;
 import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private Button btnLogin;
     private Button btnLoginGoogle;
+    private LoginButton btnLoginFacebook;
     private EditText editEmail;
     private EditText editPassword;
+
     private FirebaseAuth auth;
     private GoogleApiClient mGoogleApiClient;
+
+    // facebook
+    private CallbackManager mCallbackManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         inicializaComponentes();
+        inicializaFirebaseCallback();
         conectaGoogleApi();
         eventoClicks();
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth = Conexao.getFirebaseAuth();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                GoogleSignInAccount signInAccount = result.getSignInAccount();
+                firebaseLoginGoogle(signInAccount);
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        alert("Falha na conexão");
     }
 
     private void conectaGoogleApi() {
@@ -52,11 +95,9 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                 .build();
     }
 
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        auth = Conexao.getFirebaseAuth();
+    private void inicializaFirebaseCallback() {
+        auth = FirebaseAuth.getInstance();
+        mCallbackManager = CallbackManager.Factory.create();
     }
 
     private void eventoClicks() {
@@ -77,24 +118,43 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
             }
         });
 
+        btnLoginFacebook.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                firebaseLoginFacebook(loginResult.getAccessToken());
+            }
 
+            @Override
+            public void onCancel() {
+                alert("Operação cancelada");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                alert("Erro ao logar com com facebook");
+            }
+        });
+
+    }
+
+    private void firebaseLoginFacebook(AccessToken accessToken) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        auth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Intent intent = new Intent(LoginActivity.this, SearchMedicineActivity.class);
+                    startActivity(intent);
+                } else {
+                    alert("Erro ao conectar com o firebase");
+                }
+            }
+        });
     }
 
     private void loginGoogle() {
         Intent intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(intent, 1);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if (result.isSuccess()) {
-                GoogleSignInAccount signInAccount = result.getSignInAccount();
-                firebaseLoginGoogle(signInAccount);
-            }
-        }
     }
 
     private void firebaseLoginGoogle(GoogleSignInAccount signInAccount) {
@@ -130,11 +190,9 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         editPassword = (EditText) findViewById(R.id.login_password);
         btnLogin = (Button) findViewById(R.id.login_btn_email_pass);
         btnLoginGoogle = (Button) findViewById(R.id.login_btn_google);
-    }
 
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        alert("Falha na conexão");
+        btnLoginFacebook = (LoginButton) findViewById(R.id.login_btn_facebook);
+        btnLoginFacebook.setReadPermissions("email", "public_profile");
     }
 
     private void alert(String s) {
